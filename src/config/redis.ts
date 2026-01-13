46805da07b7195296de419b57178e137c5d4197b
mdansarijaved <javedans2003@gmail.com>
2026-01-13 18:22:49 +0530

    fix:redis client

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,12 @@
 import { createClient } from "redis";
 import type { RedisClientType } from "redis";
 
-export const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+export const redisUrl = process.env.REDIS_URL as string;
 
-export const redisClient: RedisClientType = createClient({ url: redisUrl });
+export const redisClient: RedisClientType = createClient({
+  url: redisUrl,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
